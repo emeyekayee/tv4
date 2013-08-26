330093fb99dcf1cbc6052876cdd9ca302a55ccc4
Mike Cannon <michael.j.cannon@gmail.com>
2013-08-25 17:04:36 -0700

    Check in initial create.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tv4::Application.config.secret_key_base = '5695124e75155e7098692d7d7914c1e1fb30d8091d639b54335bfd8769b95213286e16f89e5a87168156285827329856c4eaf0cb031d4f34ce403b71d00e5d9f'
